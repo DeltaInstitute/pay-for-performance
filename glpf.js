cecc85abf084c566c4e6c2128e2af1775729ef6d
colleenmcginnis <colleen.j.mcginnis@gmail.com>
2015-09-08 11:51:30 -0500

    first round of fixes

--- glpf.js
+++ glpf.js
@@ -1,19 +1,19 @@
 
         function fade(btnElement) {
             if (btnElement.value === "Back to Status Quo") {
-                document.getElementById("myImg").className = "fade-out";
- 				document.getElementById("myImg2").className = "fade-in";
- 				document.getElementById("myTxt1").className = "fade-in";
-                document.getElementById("myTxt2").className = "fade-out";
+                document.getElementById("newApproachImg").className = "fade-out";
+ 				document.getElementById("statusQuoImg").className = "fade-in";
+ 				document.getElementById("statusQuoText").className = "fade-in";
+                document.getElementById("newApproachText").className = "fade-out";
 
                 btnElement.value = "See New Approach";
 
             }
         else {
-                document.getElementById("myImg").className = "fade-in";
-				document.getElementById("myImg2").className = "fade-out";
-				document.getElementById("myTxt1").className = "fade-out";
-                document.getElementById("myTxt2").className = "fade-in";
+                document.getElementById("newApproachImg").className = "fade-in";
+				document.getElementById("statusQuoImg").className = "fade-out";
+				document.getElementById("statusQuoText").className = "fade-out";
+                document.getElementById("newApproachText").className = "fade-in";
 
                 btnElement.value = "Back to Status Quo";
 
